e3ef10f6ddfe3ae9d0570345c9c72f9bd346ff81
SamSepiolWarden <110664699+SamSepiolWarden@users.noreply.github.com>
2024-09-10 08:09:12 +0200

    Add files via upload

---
+++ Teams/Add OnlyCS-Enterprise Tag.ps1
@@ -0,0 +1,52 @@
+﻿
+# Se connecter à Microsoft Graph avec les autorisations appropriées
+Connect-MgGraph -Scopes "TeamworkTag.ReadWrite"
+
+# Importer les identifiants d'équipes à partir d'un fichier CSV
+$SearchTeam = Import-Csv -Path "C:\Test\TeamsId.csv"
+
+# enter the upn of the user 
+$UPN = Read-Host -Prompt "Enter the UPN of the user to get the information"
+
+#get the id of the use
+$UserDetails = Get-MgUser -Filter "UserPrincipalName eq '$UPN'"
+# Saisir l'identifiant de l'utilisateur
+$User = $UserDetails.Id
+
+# Si aucun utilisateur n'est spécifié, arrêter l'exécution
+if ([string]::IsNullOrEmpty($User)) {
+    Write-Host "No user id provided, stopping script..." -ForegroundColor Red
+    return
+}
+
+# Parcourir chaque identifiant d'équipe
+foreach ($Id in $SearchTeam) {
+    # Récupérer les tags d'équipe
+    $teamTags = Get-MgTeamTag -TeamId $Id.Id -All | Select-Object -Property Id, DisplayName
+
+    # Initialiser une variable pour vérifier si un tag a été trouvé
+    $tagFound = $false
+
+    # Parcourir chaque tag
+    foreach ($tag in $teamTags) {
+        if ($tag.DisplayName -eq "OnlyCS-Enterprise"){
+            $tagFound = $true
+            try {
+                New-MgTeamTagMember -TeamId $Id.Id -TeamworkTagId $tag.Id -UserId $User
+                Write-Host "User successfully added to tag" -ForegroundColor Green
+            }
+            catch {
+                Write-Host "Failed to add user to tag: $_" -ForegroundColor Red
+            }
+        }
+    }
+    
+    # Si aucun tag correspondant n'a été trouvé, afficher un message
+    if (-not $tagFound) {
+        Write-Host "No tag OnlyCS-Enterprise found" -ForegroundColor Gray
+    }
+}
+
+# Se déconnecter de Microsoft Graph
+Disconnect-MgGraph
+
---
+++ Teams/Add OnlySales Tag.ps1
@@ -0,0 +1,47 @@
+﻿
+# Se connecter à Microsoft Graph avec les autorisations appropriées
+Connect-MgGraph -Scopes "TeamworkTag.ReadWrite"
+
+# Importer les identifiants d'équipes à partir d'un fichier CSV
+$SearchTeam = Import-Csv -Path "C:\Test\TeamsId.csv"
+
+# Saisir l'identifiant de l'utilisateur
+$User = Read-Host -Prompt "Enter the object id of the user"
+
+# Si aucun utilisateur n'est spécifié, arrêter l'exécution
+if ([string]::IsNullOrEmpty($User)) {
+    Write-Host "No user id provided, stopping script..." -ForegroundColor Red
+    return
+}
+
+# Parcourir chaque identifiant d'équipe
+foreach ($Id in $SearchTeam) {
+    # Récupérer les tags d'équipe
+    $teamTags = Get-MgTeamTag -TeamId $Id.Id -All | Select-Object -Property Id, DisplayName
+
+    # Initialiser une variable pour vérifier si un tag a été trouvé
+    $tagFound = $false
+
+    # Parcourir chaque tag
+    foreach ($tag in $teamTags) {
+        if ($tag.DisplayName -eq "OnlySales"){
+            $tagFound = $true
+            try {
+                New-MgTeamTagMember -TeamId $Id.Id -TeamworkTagId $tag.Id -UserId $User
+                Write-Host "User successfully added to tag" -ForegroundColor Green
+            }
+            catch {
+                Write-Host "Failed to add user to tag: $_" -ForegroundColor Red
+            }
+        }
+    }
+    
+    # Si aucun tag correspondant n'a été trouvé, afficher un message
+    if (-not $tagFound) {
+        Write-Host "No tag OnlySales found" -ForegroundColor Gray
+    }
+}
+
+# Se déconnecter de Microsoft Graph
+Disconnect-MgGraph
+
