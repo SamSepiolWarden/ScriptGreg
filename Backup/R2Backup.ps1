0b1b5c7ba42a36d8058a0991e615fe8bc62d0bcc
Greg Semedo <gregory.semedo@safesociety.fr>
2025-01-12 00:03:28 +0100

    minor changes

--- Backup/R2Backup.ps1
+++ Backup/R2Backup.ps1
@@ -1,7 +1,8 @@
+npx wrangler login
 # Directory containing files to upload
 $sourceDir = "D:\"
 # Your bucket name
-$bucketName = "backup-greg"
+$bucketName = "music-backup"
 
 # Get all files in the source directory
 Get-ChildItem -Path $sourceDir -File -Recurse | ForEach-Object {
@@ -11,9 +12,9 @@ Get-ChildItem -Path $sourceDir -File -Recurse | ForEach-Object {
     # Convert to forward slashes for R2
     $r2Path = $relativePath.Replace("\", "/")
     
-    # Upload the file using wrangler
+    # Upload the file using wrangler - corrected command format
     Write-Host "Uploading: $($_.FullName) to $bucketName/$r2Path"
-    wrangler r2 object put "$bucketName/$r2Path" "--file=$($_.FullName)"
+    npx wrangler r2 object put $bucketName/$r2Path --file="$($_.FullName)"
 }
 
 Write-Host "Upload complete!" -ForegroundColor Green
